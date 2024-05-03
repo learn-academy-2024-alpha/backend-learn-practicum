1561601187e4e3a2c8459ae7c3cdeb7de4b3b2e8
Sarah Proctor <sproctor950@gmail.com>
2024-05-03 14:35:54 -0700

    :closed_lock_with_key: adds devise configurations

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+  include RackSession
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    sign_in(resource_name, resource)
+    render json: resource, status: :ok
+  end
+end
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+  private
+  def respond_with(current_user, _opts = {})
+    render json: current_user, status: :ok
+  end
+  def respond_to_on_destroy
+    render json: { message: "Logged out." }
+  end
+end
