fc973ae1cd2e77382e3486de4b7f0b92040fb0f2
Sarah Proctor <sproctor950@gmail.com>
2024-05-05 10:53:26 -0700

    :rotating_light: ran standardrb linting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-require 'faker'
+require "faker"
 
 5.times do
   password = Faker::Internet.password.unique
   User.where(email: Faker::Internet.email.unique).first_or_create(username: Faker::Internet.username, password: password, password_confirmation: password)
   p "created user"
 end
-
