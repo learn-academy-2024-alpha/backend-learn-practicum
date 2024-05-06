7bfec439f87b10961587db96e281fad3e2bf907c
Sarah Proctor <sproctor950@gmail.com>
2024-05-06 11:04:51 -0700

    :seedling: removes incorrect Faker syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require "faker"
 
 5.times do
-  password = Faker::Internet.password.unique
-  User.where(email: Faker::Internet.email.unique).first_or_create(username: Faker::Internet.username, password: password, password_confirmation: password)
+  password = Faker::Internet.password
+  User.where(email: Faker::Internet.email).first_or_create(username: Faker::Internet.username, password: password, password_confirmation: password)
   p "created user"
 end
