9c428befa6242a78b81b6f4f5e0cd93ec028fe7b
smorgannicole <morgann.smithh@yahoo.com>
2024-05-08 13:58:47 -0700

    corrected linting mishaps

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@ users = []
   user = User.where(email: Faker::Internet.email).first_or_create(username: Faker::Internet.username, password: password, password_confirmation: password)
   p "created user"
   users << user
-  
+
   3.times do
     user.notes.create(
       title: "#{Faker::Emotion.adjective.capitalize} message for #{Faker::FunnyName.name}",
       content: Faker::Hacker.say_something_smart,
-      public: Faker::Boolean.boolean,
+      public: Faker::Boolean.boolean
     )
     p "created note"
   end
@@ -20,7 +20,7 @@ end
 most_important_note = users[0].notes.create(
   title: "#{Faker::Emotion.adjective.capitalize} message for the team",
   content: Faker::Hacker.say_something_smart,
-  public: Faker::Boolean.boolean,
+  public: Faker::Boolean.boolean
 )
 p "created most important note"
 
@@ -28,6 +28,3 @@ users[1...users.length].each do |user|
   UserNote.create(user_id: user.id, note_id: most_important_note.id)
   puts "Associated user #{user.username} with most important note"
 end
-
-
-
