a475d65a8e578062a2057216412b7aaf649d840e
sethcrist <sethcrist50@gmail.com>
2024-05-08 15:59:23 -0700

    finished up the testing for our controllers

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -10,13 +10,67 @@ RSpec.describe "Notes", type: :request do
     end
   end
 
-  describe "GET /show" do
-    it "returns a list of private notes and a successful http response" do
-      user = User.create(email: "test@email.com", password: "password", password_confirmation: "password", username: "test")
-      note = Note.create(title: "Note 1", content: "Content 1", public: false)
-      get note_path(id: user.id)
+    describe "GET /show" do
+      it "returns a list of private notes and a successful http response" do
+        user = User.create(email: "test@email.com", password: "password", password_confirmation: "password", username: "test")
+        note = Note.create(title: "Note 1", content: "Content 1", public: false)
+        get note_path(id: user.id)
+        expect(response).to have_http_status(200)
+        expect(note).to be_valid
+      end
+    end
+
+    describe "POST #create" do
+    it "creates a valid note and a successful http response" do
+      post notes_path, params: {
+        note: {
+          title: "test",
+          content: "this is test content",
+          public: true
+        }
+      }
+      note = Note.where(title: "test").first
       expect(response).to have_http_status(200)
       expect(note).to be_valid
+    end
+  end
+
+    describe "PATCH #update" do
+    it "updates a valid note with http success" do
+      post notes_path, params: {
+        note: {
+          title: "new test",
+          content: "this is a new test content",
+          public: true
+        }
+      }
+      note = Note.where(title: "new test").first
+      patch note_path(note), params: {
+        note: {
+          title: "updated test",
+          content: "updated this is test content",
+          public: false
+        }
+      }
+      note = Note.where(title: "updated test").first
+      expect(note.title).to eq("updated test")
+      expect(note.content).to eq("updated this is test content")
+      expect(note.public).to eq(false)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+    describe "DELETE #destroy" do
+    it "deletes a note" do
+      note = Note.create(
+          title: "delete test",
+          content: "delete this is test content",
+          public: false
+      )
+      expect {
+        delete note_path(note)
+    }.to change(Note, :count).by(-1)
+    expect(response).to have_http_status(204)
     end
   end
 end
