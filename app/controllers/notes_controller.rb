a6e41a14661535fe40ce5072cfd67fa4ad26c995
Ryan <rlemus93@gmail.com>
2024-05-08 14:20:10 -0700

    added index and show on notes controller. updated notes spec. commented out pending specs

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,12 @@
 class NotesController < ApplicationController
+  def index
+    note = Note.where(public: true)
+    render json: note
+  end
+
+  def show
+    user = User.find(params[:id])
+    private_note = user.notes.where(public: false)
+    render json: private_note
+  end
 end
--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -2,6 +2,21 @@ require "rails_helper"
 
 RSpec.describe "Notes", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "returns a list of public notes and  a successful http response" do
+      note = Note.create(title: "Note 1", content: "Content 1", public: true)
+      get notes_path
+      expect(response).to have_http_status(200)
+      expect(note).to be_valid
+    end
+  end
+
+  describe "GET /show" do
+    it "returns a list of private notes and a successful http response" do
+      user = User.create(email: "test@email.com", password: "password", password_confirmation: "password", username: "test")
+      note = Note.create(title: "Note 1", content: "Content 1", public: false)
+      get note_path(id: user.id)
+      expect(response).to have_http_status(200)
+      expect(note).to be_valid
+    end
   end
 end
