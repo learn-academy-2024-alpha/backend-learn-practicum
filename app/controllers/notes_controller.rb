5874bcee39a305ef6ce742c187a6aad93f50719b
sethcrist <sethcrist50@gmail.com>
2024-05-08 15:56:36 -0700

    finished the create, update, and destroy methods

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,4 +9,29 @@ class NotesController < ApplicationController
     private_note = user.notes.where(public: false)
     render json: private_note
   end
+
+  def create
+    note = Note.create(note_params)
+      if note.valid?
+        render json: note
+    end
+  end
+
+  def update
+    note = Note.find(params[:id])
+    note.update(note_params)
+    if note.valid?
+      render json: note
+    end
+  end
+
+  def destroy
+    note = Note.find(params[:id])
+    note.destroy
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :content, :public)
+  end
 end
