df78a5e515d5c942d611e19e11b56a15b7c972d0
Louie-cpu <luis.u.m779@gmail.com>
2024-05-08 21:59:35 -0700

    super clean code

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,16 +1,16 @@
-require "rails_helper"
+require 'rails_helper'
 
 describe Note, type: :model do
   describe "validations" do
     it "validates presence of title" do
       note = Note.new(content: "text", public: false)
       expect(note).not_to be_valid
-      expect(note.errors[:title]).to include("can"t be blank")
+      expect(note.errors[:title]).to include("can't be blank")
     end
     it "validates presence of content" do
       note = Note.new(title: "Title", public: false)
       expect(note).not_to be_valid
-      expect(note.errors[:content]).to include("can"t be blank")
+      expect(note.errors[:content]).to include("can't be blank")
     end
     it "validates inclusion of public" do
       note = Note.new(title: "Title", content: "text", public: true)
@@ -32,7 +32,7 @@ describe Note, type: :model do
       expect(association.macro).to eq(:has_many)
       expect(association.options[:through]).to eq(:user_notes)
     end
-    it should accept nested attributes for user_notes" do
+    it "should accept nested attributes for user_notes" do
       expect(Note.new).to respond_to(:user_notes_attributes=)
     end
   end
