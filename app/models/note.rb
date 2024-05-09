24c69185c3197a22013e29014752e1c018ef7fd8
Louie-cpu <luis.u.m779@gmail.com>
2024-05-08 21:46:26 -0700

    even cleaner code

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,9 +1,7 @@
 class Note < ApplicationRecord
   has_many :user_notes
   has_many :users, through: :user_notes
-
   validates :title, :content, presence: true
-  validates :public, inclusion: { in: [true, false] }
-
+  validates :public, inclusion: {in: [true, false]}
   accepts_nested_attributes_for :user_notes
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,19 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Note, type: :model do
-  describe 'validations' do
-    it 'validates presence of title' do
-      note = Note.new(content: 'text', public: false)
+  describe "validations" do
+    it "validates presence of title" do
+      note = Note.new(content: "text", public: false)
       expect(note).not_to be_valid
-      expect(note.errors[:title]).to include("can't be blank")
+      expect(note.errors[:title]).to include("can"t be blank")
     end
-    it 'validates presence of content' do
-      note = Note.new(title: 'Title', public: false)
+    it "validates presence of content" do
+      note = Note.new(title: "Title", public: false)
       expect(note).not_to be_valid
-      expect(note.errors[:content]).to include("can't be blank")
+      expect(note.errors[:content]).to include("can"t be blank")
     end
-    it 'validates inclusion of public' do
-      note = Note.new(title: 'Title', content: 'text', public: true)
+    it "validates inclusion of public" do
+      note = Note.new(title: "Title", content: "text", public: true)
       expect(note).to be_valid
       note.public = false
       expect(note).to be_valid
@@ -22,17 +22,17 @@ describe Note, type: :model do
       expect(note.errors[:public]).to include("is not included in the list")
     end
   end
-  describe 'associations' do
-    it 'should have many user_notes' do
+  describe "associations" do
+    it "should have many user_notes" do
       association = described_class.reflect_on_association(:user_notes)
       expect(association.macro).to eq(:has_many)
     end
-    it 'should have many users through user_notes' do
+    it "should have many users through user_notes" do
       association = described_class.reflect_on_association(:users)
       expect(association.macro).to eq(:has_many)
       expect(association.options[:through]).to eq(:user_notes)
     end
-    it 'should accept nested attributes for user_notes' do
+    it should accept nested attributes for user_notes" do
       expect(Note.new).to respond_to(:user_notes_attributes=)
     end
   end
