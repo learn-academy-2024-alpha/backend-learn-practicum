fef720f1f356aa98997a72f0d4677606307f761c
Ryan <rlemus93@gmail.com>
2024-05-09 12:01:42 -0700

    added create method and spec

--- app/controllers/user_notes_controller.rb
+++ app/controllers/user_notes_controller.rb
@@ -1,2 +1,21 @@
 class UserNotesController < ApplicationController
+
+  def create 
+    username = params[:user_note][:user_id]
+    user = User.where(username: username).first
+    params[:user_note][:user_id] = user.id 
+    user_note = UserNote.create(user_note_params)
+    if user_note.valid?
+      render json: user_note
+    else
+      render json: user_note.errors, status: 422
+    end
+  end
+
+end
+
+
+private 
+def user_note_params
+  params.require(:user_note).permit(:note_id, :user_id)
 end
